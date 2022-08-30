382493afb3f35ebedffdfde45d447090bf90ceee
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-30 08:23:59 +0300

    connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 
-app.listen(5000, () => {
-    console.log('Server has been started...')
+app.get('/', (req, res) => {
+    return res.json({ message: 'Everything is ok!'})
 })
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://Kettengeschwindigkeit:1488@cluster0.wvgxwjm.mongodb.net/eng-dictionary?retryWrites=true&w=majority')
+        app.listen(5000, () => console.log('Server has been started...'))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
