a247d0b620931858e1b660e400584f65b8ec2a90
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-31 09:59:40 +0300

    initialized project front-end part, installed tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const Header = () => <h1>Header</h1>
+const MainContent = () => <h1>Main Content</h1>
+const Footer = () => <h1>Footer</h1>
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
+
+export default App
