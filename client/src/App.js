52ff720691bab095438babda4f587c6adc87474f
Kettengeschwindigkeit <666damned999@mail.ru>
2022-09-18 10:30:21 +0300

    [front-end] - set registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
+import { ToastContainer } from "react-toastify"
 import { Layout } from "./components/Layout"
 import { LoginPage } from "./pages/LoginPage"
 import { MainPage } from "./pages/MainPage"
 import { RegisterPage } from "./pages/RegisterPage"
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
       </Routes>
+      <ToastContainer position="bottom-right" />
     </Layout>
   )
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,18 +1,59 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import { toast } from "react-toastify"
+import { registerUser } from "../redux/features/auth/authSlice"
 
 export function RegisterPage() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const { status } = useSelector((state) => state.auth)
+
+    const dispatch = useDispatch()
+    console.log(status)
+    const handleSubmit = () => {
+        try {
+            dispatch(registerUser({ email, password }))
+            setEmail("")
+            setPassword("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        if (status) {
+            toast(status)
+        }
+    }, [status])
+
     return (
         <form className="w-1/4 h-60 mx-auto mt-40" onSubmit={e => e.preventDefault()}>
             <h1 className="text-lg text-center text-gray-600">Registration</h1>
             <label className="text-xs text-gray-600">Email:
-                <input type="text" className="w-full mt-1 py-1 px-2 border border-gray-400 text-xs text-black bg-gray-300 rounded outline-none" />
+                <input
+                    type="text"
+                    className="w-full mt-1 py-1 px-2 border border-gray-400 text-xs text-black bg-gray-300 rounded outline-none"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </label>
             <label className="text-xs text-gray-600">Password:
-                <input type="password" className="w-full mt-1 py-1 px-2 border border-gray-400 text-xs text-black bg-gray-300 rounded outline-none" />
+                <input
+                    type="password"
+                    className="w-full mt-1 py-1 px-2 border border-gray-400 text-xs text-black bg-gray-300 rounded outline-none"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </label>
             <div className="flex gap-8 justify-center mt-4">
-                <button type="submit" className="flex justify-center items-center px-4 py-2 rounded text-sm bg-gray-400 border border-gray-500">Sign Up</button>
+                <button
+                    type="submit"
+                    className="flex justify-center items-center px-4 py-2 rounded text-sm bg-gray-400 border border-gray-500"
+                    onClick={handleSubmit}
+                >
+                    Sign Up
+                </button>
                 <Link to="/login" className="flex justify-center items-center text-sm text-gray-600">Already have account?</Link>
             </div>
         </form>
